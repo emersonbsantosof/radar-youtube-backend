bdb4017815bd0c185c0d9079cec93833907ca3fe
emersonbsantosof <emersonbsantos@gmail.com>
2025-07-06 12:42:31 -0300

    Add core dependencies manually to requirements.txt and clean app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from flask import Flask, jsonify, request
+﻿from flask import Flask, jsonify, request  
 from googleapiclient.discovery import build
 from flask_cors import CORS # Importe CORS para permitir requisições do seu frontend
 
